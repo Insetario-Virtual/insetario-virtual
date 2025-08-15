192c62ac664a23ce2fed3025700cc63683133c61
Luiz E. Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2025-08-14 22:03:25 -0300

    Initial models -a structures

---
+++ app/Http/Controllers/InsectController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Insect;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreInsectRequest;
+use App\Http\Requests\UpdateInsectRequest;
+
+class InsectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreInsectRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Insect $insect)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Insect $insect)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateInsectRequest $request, Insect $insect)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Insect $insect)
+    {
+        //
+    }
+}
