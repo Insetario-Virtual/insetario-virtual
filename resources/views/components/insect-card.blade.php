905aefaf60e8d6ceb269fc08a03d2d7ac3425d0d
Luiz E. Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2025-10-14 20:07:47 -0300

    Geral style

--- resources/views/components/insect-card.blade.php
+++ resources/views/components/insect-card.blade.php
@@ -1,16 +1,23 @@
 @props(['id', 'common_name', 'scientific_name', 'image_path' => null])
 
-
 <a href="{{ route('insetario.show', ['id' => $id]) }}"
-
-    class="block rounded-2xl shadow-md overflow-hidden hover:shadow-lg transition">
-    <div class="h-40 bg-gray-100 overflow-hidden">
+    class="flex h-32 sm:h-36 rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition duration-300 transform hover:-translate-y-0.5 bg-white/95 max-w-3xl mx-auto">
+    
+    <div class="w-32 h-full bg-gray-100 overflow-hidden flex-shrink-0">
         <img
             src="{{ asset("/" . $image_path) }}"
-            alt="{{ $scientific_name }}" class="w-full h-full object-cover">
+            alt="{{ $scientific_name }}" 
+            class="w-full h-full object-cover">
     </div>
-    <div class="p-4">
-        <h3 class="text-lg font-semibold text-gray-800">{{ $common_name }}</h3>
-        <p class="text-sm text-gray-600 italic">{{ $scientific_name }}</p>
+    
+    <div class="p-3 sm:p-4 flex flex-col justify-center w-full h-full overflow-hidden">
+        
+        <h3 class="text-lg font-semibold text-gray-800 capitalize leading-tight truncate">
+            {{ $common_name }}
+        </h3>
+        
+        <p class="text-sm text-gray-600 italic leading-tight mt-1 line-clamp-2 overflow-hidden">
+            {{ $scientific_name }}
+        </p>
     </div>
 </a>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 <div class="pb-4 w-full h-fit px-10 max-sm:px-4 bg-fixed bg-no-repeat bg-cover bg-center">
 
-    <h1 class="pt-4 text-3xl font-bold text-white mb-6 md:text-4xl">Informações do Projeto</h1>
+    <h1 class="pt-20 text-3xl font-bold text-white mb-6 md:text-4xl">Informações do Projeto</h1>
 
     <div class="w-full h-fit rounded py-3 z-10">
 
         @foreach ($siteContent as $content)
-        <section class="mb-8">
-            <h2 class="mt-5 text-xl font-semibold text-white md:text-2xl">{{ $content->title }}</h2>
+        <section class="mb-2">
+            <h2 class="mt-2 text-xl font-semibold text-white md:text-2xl">{{ $content->title }}</h2>
 
             <p class="w-full text-justify text-white pl-0 pt-3 pb-4 text-base md:pl-12 md:text-lg">
                 {{ $content->description }}
